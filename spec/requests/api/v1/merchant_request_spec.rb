6ebcaed89624541ddb49cb8cd8c20d92319721ca
Ty Mazey <tymazey@gmail.com>
2019-03-12 13:50:07 -0600

    show pages for merchant endpoint completed

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -11,4 +11,15 @@ describe 'Merchants Api' do
     merchants = JSON.parse(response.body)
     expect(merchants["data"].count).to eq(3)
   end
+
+  it 'can get a single merchant by its id' do
+    id = create(:merchant).id.to_s
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant['data']['id']).to eq(id)
+  end
 end
