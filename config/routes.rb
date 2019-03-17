c4627260cee799430ea84110dd1dc9895ed04699
Ty Mazey <tymazey@gmail.com>
2019-03-17 12:51:08 -0600

    adds finder endpoints for invoices

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,11 @@ Rails.application.routes.draw do
         get ':id/favorite_customer', to: 'customers#show'
       end
       resources :merchants, only: [:index, :show]
+      namespace :invoices do
+        get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+        get 'random', to: 'random#show'
+      end
       resources :invoices, only: [:index, :show]
     end
   end
--- spec/requests/api/v1/invoice_request_spec.rb
+++ spec/requests/api/v1/invoice_request_spec.rb
@@ -13,8 +13,8 @@ describe 'invoices API ' do
     get '/api/v1/invoices'
 
     expect(response).to be_successful
-    merchants = JSON.parse(response.body)
-    expect(merchants["data"].count).to eq(3)
+    invoices = JSON.parse(response.body)
+    expect(invoices["data"].count).to eq(3)
   end
 
   it 'can get a single merchant by its id' do
@@ -24,9 +24,76 @@ describe 'invoices API ' do
 
     get "/api/v1/invoices/#{id}"
 
-    merchant = JSON.parse(response.body)
+    json = JSON.parse(response.body)
 
     expect(response).to be_successful
-    expect(merchant['data']['id']).to eq(id)
+    expect(json['data']['id']).to eq(id)
+  end
+
+  it 'can find a invoice by id paramaters' do
+    cust = create(:customer).id
+    merch = create(:merchant).id
+    id = create(:invoice, merchant_id: merch, customer_id: cust).id
+
+    get "/api/v1/invoices/find?id=#{id}"
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(json["data"]["attributes"]["id"]).to eq(id)
+  end
+
+  it 'can find a invoice by customer_id paramaters' do
+    cust = create(:customer).id
+    merch = create(:merchant).id
+    id = create(:invoice, merchant_id: merch, customer_id: cust).id
+
+    get "/api/v1/invoices/find?customer_id=#{cust}"
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(json["data"]["attributes"]["customer_id"]).to eq(cust)
+  end
+
+  it 'can find a invoice by merchant_id paramaters' do
+    cust = create(:customer).id
+    merch = create(:merchant).id
+    id = create(:invoice, merchant_id: merch, customer_id: cust).id
+
+    get "/api/v1/invoices/find?merchant_id=#{merch}"
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(json["data"]["attributes"]["merchant_id"]).to eq(merch)
+  end
+
+  it 'can find a invoice by created_at paramaters' do
+    date = "2012-03-27 14:56:04 UTC"
+    cust = create(:customer).id
+    merch = create(:merchant).id
+    id = create(:invoice, merchant_id: merch, customer_id: cust, created_at: date).id
+
+    get "/api/v1/invoices/find?created_at=#{date}"
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(json["data"]["attributes"]["created_at"]).to eq("2012-03-27T14:56:04.000Z")
+  end
+
+  it 'can find a invoice by created_at paramaters' do
+    date = "2012-03-27 14:56:04 UTC"
+    cust = create(:customer).id
+    merch = create(:merchant).id
+    id = create(:invoice, merchant_id: merch, customer_id: cust, updated_at: date).id
+
+    get "/api/v1/invoices/find?updated_at=#{date}"
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(json["data"]["attributes"]["updated_at"]).to eq("2012-03-27T14:56:04.000Z")
   end
 end
